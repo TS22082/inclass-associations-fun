5007425c4997f6e1a50aaf32634d1d4b2c49de19
TS22082 <ts22082@gmail.com>
2020-01-08 20:39:50 -0800

    add client routes and basic html

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./views"));
 
 const authorRoutes = require("./controllers/api/author-routes");
 app.use("/api/author", authorRoutes);
 
 const postRoutes = require("./controllers/api/post-routes");
 app.use("/api/post", postRoutes);
+
+const clientRoutes = require("./controllers/client/html-routes");
+app.use("/", clientRoutes);
 
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
