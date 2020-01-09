99b7384c58d747ca18c5e9ba14d55f403df320de
TS22082 <ts22082@gmail.com>
2020-01-08 19:45:38 -0800

    finish post route and add to server

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(express.json());
 const authorRoutes = require("./controllers/api/author-routes");
 app.use("/api/author", authorRoutes);
 
+const postRoutes = require("./controllers/api/post-routes");
+app.use("/api/post", postRoutes);
+
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`App listening: http://localhost:${PORT}`);
